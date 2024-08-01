f89198e16f1a97e7f8deb5cee8e843e2b58bd11f
Rukayat Jimoh <107076451+RukayatJimoh@users.noreply.github.com>
2024-08-01 20:35:41 +0200

    Update data_lake.tf

--- data_lake.tf
+++ data_lake.tf
@@ -1,8 +1,7 @@
-# resource "aws_s3_bucket" "example" {
-#   bucket = "bimmy-tf-test-bucket"
-# }
+ resource "aws_s3_bucket" "example" {
+   bucket = "bimmy-tf-test-bucket"
+ }
 
-# module "firstmodule" {
-#   source = "./module/s3"
-#   bucket_name = "bimmy-module-bucket"
-# }
+ module "firstmodule" {
+   source = "./module/s3"
+   bucket_name = "bimmy-module-bucket" }

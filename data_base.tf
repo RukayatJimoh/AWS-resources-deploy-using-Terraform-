2957c412b8c5028b5e7ccafab9635ec60493087e
Rukayat Jimoh <107076451+RukayatJimoh@users.noreply.github.com>
2024-08-01 20:35:13 +0200

    Update data_base.tf

--- data_base.tf
+++ data_base.tf
@@ -1,15 +1,3 @@
-# resource "aws_db_instance" "default" {
-#   allocated_storage    = 10
-#   db_name              = "mydb"
-#   engine               = "mysql"
-#   engine_version       = "5.7"
-#   instance_class       = "db.t3.micro"
-#   username             = "foo"
-#   password             = "foobarbaz"
-#   parameter_group_name = "default.mysql5.7"
-#   skip_final_snapshot  = true
-# }
-
 resource "random_password" "bimmy_company" {
   length           = 20
   special          = false
